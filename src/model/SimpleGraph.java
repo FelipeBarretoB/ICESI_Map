6c472275b8abcc520f23ddac488350fc260851ec
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2021-11-11 22:14:01 -0500

    SimpleGraph's TAD updated

--- src/model/SimpleGraph.java
+++ src/model/SimpleGraph.java
@@ -1,5 +1,48 @@
 package model;
 
-public class SimpleGraph {
+import java.util.ArrayList;
 
+public class SimpleGraph {
+	private ArrayList<Vertex> nodes;
+	private ArrayList<Edge> edges;
+	
+	public SimpleGraph() {
+		nodes = new ArrayList<Vertex>();
+		edges = new ArrayList<Edge>();
+	}
+	
+	public ArrayList<Vertex> getNodes() {
+		return nodes;
+	}
+	
+	public void setNodes(ArrayList<Vertex> nodes) {
+		this.nodes = nodes;
+	}
+	
+	public ArrayList<Edge> getEdges() {
+		return edges;
+	}
+	
+	public void setEdges(ArrayList<Edge> edges) {
+		this.edges = edges;
+	}
+	
+	public void BFS(Vertex init) {
+		
+	}
+	
+	public void DFS() {
+		
+	}
+	
+	public void Djikstra(Vertex init, Vertex end) {
+		
+	}
+	
+	public void FloydWarshall() {
+		
+	}
+
+	
+	
 }
