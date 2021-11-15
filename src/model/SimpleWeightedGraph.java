63a2b2cdfc0731f40272691998036e9cb89b20ac
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2021-11-14 22:30:06 -0500

    TI3 Primera Entrega

--- src/model/SimpleWeightedGraph.java
+++ src/model/SimpleWeightedGraph.java
@@ -1,5 +1,13 @@
 package model;
 
-public class SimpleWeightedGraph {
-
+public class SimpleWeightedGraph<T> extends Graph<T>{
+	
+	public SimpleWeightedGraph() {
+		super();
+	}
+	
+	public void addEdge(Vertex<T> origin, Vertex<T> destiny, int weight) {
+		Edge<T> e = new Edge<>(weight, origin, destiny);
+		getEdges().add(e);
+	}
 }
