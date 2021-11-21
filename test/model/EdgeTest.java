9cea3008b0ede1c9299474434d239c942231dd9f
Martin Cifuentes <marcisego@gmail.com>
2021-11-21 16:39:17 -0500

    2 jUnit Tests

--- test/model/EdgeTest.java
+++ test/model/EdgeTest.java
@@ -1,5 +1,27 @@
 package model;
 
-public class EdgeTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EdgeTest {
+
+	private Edge<Integer> edge;
+	private Vertex<Integer> vertexA;
+	private Vertex<Integer> vertexB;
+	
+	public void setupScenario() {
+		vertexA = new Vertex<Integer>(5,"a");
+		vertexB= new Vertex<Integer>(5,"a");
+		edge = new Edge<Integer>(2,vertexA,vertexB);
+	}
+	
+	@Test
+	public void testConstructor() {
+		setupScenario();
+		assertEquals(edge.getWeight(), 2);
+		assertEquals(edge.getOrigin(), vertexA);
+		assertEquals(edge.getDestiny(), vertexB);
+	}
 
 }
--- test/model/VertexTest.java
+++ test/model/VertexTest.java
@@ -1,5 +1,22 @@
 package model;
 
-public class VertexTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
+class VertexTest {
+
+	private Vertex<Integer> vertex;
+	
+	
+	public void setupScenario() {
+		vertex = new Vertex<Integer>(5,"a");
+	}
+	
+	@Test
+	public void testConstructor() {
+		setupScenario();
+		assertEquals(vertex.getValue(), 5);
+		assertEquals(vertex.getProperties(), "a");
+	}
 }
